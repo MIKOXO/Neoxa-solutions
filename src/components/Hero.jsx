75612bae50b278e6254079ce7ea809a2a4d0f4b9
MIKOXO <mikermount19@yahoo.com>
2025-10-24 18:11:17 +0300

    Padding fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import heroIllustration from "../assets/hero-illustration.png";
 
 const Hero = () => {
   return (
-    <section className="min-h-screen bg-lightBg dark:bg-DarkBg transition-colors duration-300">
+    <section className="pb-5 bg-LightBg dark:bg-DarkBg transition-colors duration-300">
       <div className="mx-auto container pt-7 lg:pt-20 px-5 lg:px-10 flex flex-col-reverse lg:flex-row items-center justify-between">
         {/* Text Content */}
         <div className="text-LightText dark:text-DarkText animate-fade-in text-center lg:text-left">
